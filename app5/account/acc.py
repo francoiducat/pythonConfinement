7bec03f6767960fdd2a019e06053e3269299abc3
Francois DUCATILLON <francois.ducatillon@decathlon.com>
2020-04-10 10:33:49 +0200

    OOP glossary

--- app5/account/acc.py
+++ app5/account/acc.py
@@ -18,20 +18,34 @@ class Account:
 
 class Checking(Account):
 
+    # Doc
+
+    """Doc of checking class goes here"""
+
+    # Type
+
+    type = "checking"
+
+    # Constructor
+
     def __init__(self, filepath, fee):
         Account.__init__(self, filepath)
         self.fee = fee
 
+    # Method
     def transfer(self, amount):
         self.balance = self.balance - amount - self.fee
 
 
+# instance of checking class
 checking = Checking("balance.txt", 1)
 # checking.deposit(10)
 checking.transfer(100)
 checking.commit()
 print(checking.balance)
 
+print(checking.type)
+print(checking.__doc__)
 
 # account = Account("balance.txt")
 # print(account.balance)
